89015ab3fcec33bed964bc25eaabe027fb53543f
Tarak <ramarao.study@gmail.com>
2016-12-21 10:01:14 +0530

    c

--- src/main/java/org/tarak/pms/controllers/CategoryController.java
+++ src/main/java/org/tarak/pms/controllers/CategoryController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.tarak.pms.models.Category;
 import org.tarak.pms.services.ServiceInterface;
 
