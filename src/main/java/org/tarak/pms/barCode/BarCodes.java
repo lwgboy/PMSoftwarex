350a931e1e0cc69edd0cda5296a24360d1d76a27
Tarak <ramarao.study@gmail.com>
2017-02-04 17:16:46 +0530

    Bar Code

--- src/main/java/org/tarak/pms/barCode/BarCodes.java
+++ src/main/java/org/tarak/pms/barCode/BarCodes.java
@@ -17,7 +17,7 @@ import com.itextpdf.text.pdf.PdfWriter;
 public class BarCodes {
     public static final String DEST = "barcode_table.pdf";
  
-    public static void main(String[] args) throws IOException,
+    public static void mainx(String[] args) throws IOException,
             DocumentException {
        /* File file = new File(DEST);
         file.getParentFile().mkdirs();*/
